408281d129abeb7e02ef437db14067a4bf62be4a
Paola Accioly <prga@cin.ufpe.br>
2015-07-16 12:58:31 -0300

    adding new files for tests

---
+++ A.java
@@ -0,0 +1,84 @@
+public class A {
+	
+	int aa;
+	int z;
+	int c;
+	int b;
+	int a;
+	int bb;
+	//adding ... u0000 and u07FF in order to test sed commands
+	String foo = null;
+	
+	public void a(){
+		int a;
+		int b;
+		int c;
+		int y;
+		int m;
+		int n;
+	}
+	
+	public void amo(){
+		
+		String m = "amo";
+		m = m + "oma";
+		int x = 1;
+		x = x + 1;
+		int z = 3;
+	}
+	
+	public void mo(){
+		int meme = 89;
+	}
+	
+	public void mob(){
+		
+		String m = "mob";
+
+		int y;
+
+		int x;
+
+	}
+	
+	public void b(){
+		
+		int p;
+		int q;
+		int r;
+		p = 0;
+		q = p +1;
+	}
+	
+	public void bc(){
+		int x;
+		x = 1;
+	}
+	
+	public void c(){
+		
+		int r = 0;
+		String c = "c: " + r;
+
+		int y;
+
+
+	}
+	
+	public void cme(){
+		
+		String m = "";
+		
+	}
+	
+	public int me(){
+		int aa = 1;
+		int bb = 2;
+		int x;
+		int c;
+		
+		return aa;
+	}
+	
+	
+}
